c9facb37c76a9701b7ec0bac7742d7d116632249
mikle <mr.mikle@mail.ru>
2023-01-11 15:50:37 +0300

    понял как отрабатывают кнопки, как считать текст.. мучаем как заполнить comboBOX как мне надо

--- main.py
+++ main.py
@@ -4,22 +4,30 @@ from PySide6.QtGui import QFontDatabase
 from mvp import Ui_MainWindow
 from pytube import YouTube
 
+
 class DownLoader(QMainWindow):
-   def __init__(self):
-      super(DownLoader, self).__init__()
-      self.ui = Ui_MainWindow()
-      self.ui.setupUi(self)
+    def __init__(self):
+        super(DownLoader, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
-      self.ui.pushButton.clicked.connect(self.test)
+        self.ui.pushButton.clicked.connect(self.sourse)
+        self.ui.pushButton_2.clicked.connect(self.downloads)
 
-   def test(self) -> None:
-      yt = YouTube('https://youtu.be/q3ma5waVGb0')
-      videos = yt.streams.filter(file_extension='mp4')
-      for v in videos:
-         print(v.itag)
-      stream = yt.streams.get_by_itag(18)
-      stream.download()
+    def sourse(self) -> None:
+        yt = YouTube('https://youtu.be/q3ma5waVGb0')
+        videos = yt.streams.filter(file_extension='mp4')
 
+        for v in videos:
+            print(v.resolution)
+            self.ui.comboBox.addItems(v.resolution)
+            
+         # stream = yt.streams.get_by_itag(18)
+         # stream.download()
+
+    def downloads(self):
+      print(self.ui.plainTextEdit.toPlainText())
+      
 
 
 if __name__ == "__main__":
@@ -29,17 +37,3 @@ if __name__ == "__main__":
     window.show()
 
     sys.exit(app.exec())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
